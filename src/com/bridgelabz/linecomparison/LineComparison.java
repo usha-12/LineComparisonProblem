f6b590255b80ac99be3610efdcf9819f85995c2d
usha-12 <usha12ahirwar@gmail.com>
2023-01-17 15:32:54 +0530

    Added: Welcome to the line comparison program

--- src/com/bridgelabz/linecomparison/LineComparison.java
+++ src/com/bridgelabz/linecomparison/LineComparison.java
@@ -1,2 +1,7 @@
-package com.bridgelabz.linecomparison;public class LineComparison {
+package com.bridgelabz.linecomparison;
+
+public class LineComparison {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Line Comparison Computation Program");
+    }
 }
