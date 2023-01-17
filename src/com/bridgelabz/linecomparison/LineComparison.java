b00363fca4a95b81b57cb4565e96dbd037919937
usha-12 <usha12ahirwar@gmail.com>
2023-01-17 15:45:02 +0530

    Added: Comparing if a line is equal to, greater than or less than another line

--- src/com/bridgelabz/linecomparison/LineComparison.java
+++ src/com/bridgelabz/linecomparison/LineComparison.java
@@ -30,8 +30,10 @@ public class LineComparison {
         line1x2 = sc.nextInt();
         System.out.println("Enter the ordinate (y-coordinate) : ");
         line1y2 = sc.nextInt();
+
         lengthLine1 = Math.sqrt((Math.pow((line1x2-line1x1), 2)+Math.pow((line1y2-line1y1), 2)));
         System.out.println("Length of Line 1 is : " + lengthLine1);
+
         System.out.println();
         System.out.println("Using the Cartesian Coordinate System, enter the endpoints of the Line 2");
         System.out.println("Endpoint 1 (x1, y1)");
@@ -44,20 +46,30 @@ public class LineComparison {
         line2x2 = sc.nextInt();
         System.out.println("Enter the ordinate (y-coordinate) : ");
         line2y2 = sc.nextInt();
+
         lengthLine2 = Math.sqrt((Math.pow((line2x2-line2x1), 2)+Math.pow((line2y2-line2y1), 2)));
         System.out.println("Length of Line 2 is : " + lengthLine2);
         //check equality of two lines based on the end points, whether two lines are the equal
         Double d1 = new Double(lengthLine1);
         Double d2 = new Double(lengthLine2);
         if (d1.equals(d2))
-            System.out.print("Line 1 and line 2 have Equal lengths.");
+            System.out.println("Line 1 and line 2 have Equal lengths.");
         else
-            System.out.print("Line 1 and Line 2 do not have Equal lengths.");
+            System.out.println("Line 1 and Line 2 do not have Equal lengths.");
+        //compare two lines based on the end points, So that I know one line is equal, greater or less than the other line.
+        if ((Double.compare(lengthLine1, lengthLine2)) == 0)
+            System.out.println("Length of Line 1 is equal to the length of Line 2.");
+        else
+        if ((Double.compare(lengthLine1, lengthLine2)) < 0)
+            System.out.println("Length of Line 1 is less than the length of Line 2.");
+        else
+        if ((Double.compare(lengthLine1, lengthLine2)) > 0)
+            System.out.println("Length of Line 1 is greater than the length of Line 2.");
     }
 }
 /*As a fan of geometry, I want to
-check equality of two lines
-based on the end points, So
-that I know when two lines are
-the equal. - Using Java equals method to check equality of 2 Lengths is
+compare two lines based on
+the end points, So that I know
+one line is equal, greater or
+less than the other line. - Using Java compareTo method to compare 2 Lengths is
 preferable.*/
